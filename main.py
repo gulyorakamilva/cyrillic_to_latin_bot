c8671e03118daf4b3dcf0df9afaef9d99ae37f5f
gulyorakamilva <gulyorakomilova23.gmail.com>
2026-05-19 18:18:09 +0500

    Refactor main.py for improved readability and consistency

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import telebot
 import os
 from dotenv import load_dotenv
@@ -5,29 +6,26 @@ from transliterate import to_cyrillic, to_latin
 
 load_dotenv()
 
-TOKEN=os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN, parse_mode=None) 
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "Assalomu aleykum xush kelibsiz! Sizga qanday yozdam kerak?")
-  
+	bot.reply_to(message, "Assalom alaykum, botimizga xush kelibsiz")
+	
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-  text=message.text
-  if text.isascii():
-    bot.reply_to(message, to_cyrillic(text))
-  else:
-      bot.reply_to(message,to_latin(text))  
-
-
+	# print(message)
+	text = message.text
+	if text.isascii():
+		bot.reply_to(message, to_cyrillic(text))
+	else:
+		bot.reply_to(message, to_latin(text))
+	
 bot.infinity_polling()
 
 # s = input()
 # if s.isascii():
-#     print(transliterate.to_cyrillic(s))
+#     print(to_cyrillic(s))
 # else:
-#   print(transliterate.to_latin(s))
-
-
-
+#     print(to_latin(s))
