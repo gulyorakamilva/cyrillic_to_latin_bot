ac2f4ebde993d184cb804ea7c972d79ce29b122e
gulyorakamilva <gulyorakomilova23.gmail.com>
2026-05-23 17:23:15 +0500

    Update welcome message and improve response handling in main.py

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	# bot.reply_to(message, "Howdy, how are you doing?")
+	text = "Assalomu aleykum, meni portfolio botimga xush kelibsz."
 	keyboard = types.ReplyKeyboardMarkup()
 	btn1 = types.KeyboardButton('About me')
 	btn2 = types.KeyboardButton('Contact')
 	keyboard.add(btn1, btn2)
-	bot.send_message(message.chat.id, "Assalomu aleykum", reply_markup=keyboard)
-	bot.send_message(message.chat.id, "Assalomu aleykum")
+	bot.send_message(message.chat.id, text, reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	if message.text == "About me":
+		bot.send_message(message.chat.id, "men Komilova Gulyora\nMen 16 yoshli talaba bo'lib, dasturlashga qiziqaman. Hozirda Python tilini o'rganmoqdaman.")
+	elif message.text == "Contact":
+		bot.send_message(message.chat.id, "Bu qism tez orada qo`shiladi.")
 
 bot.infinity_polling()
